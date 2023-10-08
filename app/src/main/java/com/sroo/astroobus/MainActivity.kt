3cd5cbc5efb0cc0a8ed34c31fc1c085cc576380c
obertcoy <laisobert2@gmail.com>
2023-10-09 01:15:38 +0700

    user login UI

---
+++ app/src/main/java/com/sroo/astroobus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sroo.astroobus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_register)
+    }
+}
