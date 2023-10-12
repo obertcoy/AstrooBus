afdbf3c5f4656992d0a74dfc56bea81148befe49
StefanieSheryl <85792580+StefanieSheryl@users.noreply.github.com>
2023-10-12 08:50:23 +0700

    OnProgress

--- app/src/main/java/com/sroo/astroobus/repository/RegisterRepository.kt
+++ app/src/main/java/com/sroo/astroobus/repository/RegisterRepository.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.sroo.astroobus.database.FirebaseInitializer
@@ -17,10 +18,10 @@ class RegisterRepository (){
     private var firebase: FirebaseDatabase?
     private var auth: FirebaseAuth?
     private lateinit var databaseReference: DatabaseReference
+    private val adapter = AdapterHelper()
     init {
         FirebaseInitializer.initialize()
         firebase = FirebaseInitializer.instance?.getDatabase()
-        databaseReference = firebase!!.getReference("Users")
         auth = FirebaseInitializer.instance?.getAuth()
     }
 
@@ -54,21 +55,51 @@ class RegisterRepository (){
 
     fun addUserToDatabase(user: User, context: Context) {
         //change to hashmap
-        val adapter = AdapterHelper()
         val userMap = adapter.UserToHashmap(user)
-
+        databaseReference = firebase!!.getReference("Users")
         user!!.uid?.let {
             databaseReference.child(it).setValue(userMap)
                 .addOnSuccessListener {
-                    Log.d("Register Repository", "Success")
+                    Log.d("Register Repository", "Success Adding User")
                 }
                 .addOnFailureListener { error ->
-                    Log.e("Register Repository", "Fail: ${error.message}", error)
+                    Log.e("Register Repository", "Fail Adding User: ${error.message}", error)
                 }
         }
     }
 
     fun addTempUserToDatabase(phoneNum :String, code: String){
+        val phoneMap = adapter.phoneHashMap(phoneNum, code)
+        databaseReference = firebase!!.getReference("TempUsers")
+        databaseReference.child(phoneNum).setValue(phoneMap)
+            .addOnSuccessListener {
+                Log.d("Register Repository", "Success Adding Temp User")
+            }
+            .addOnFailureListener { error ->
+                Log.e("Register Repository", "Fail Adding Temp User: ${error.message}", error)
+            }
+    }
+
+    fun findTempUserByPhoneNum(phoneNum: String):Int{
+        databaseReference = firebase!!.getReference("TempUsers")
+        try {
+            val snapshot = databaseReference.child(phoneNum).get()
+
+            if (snapshot != null) {
+                val tempUserMap = snapshot.value as Map<String, Any>
+                val code = tempUserMap["code"].toString()
+                code // Return the code
+            } else {
+                null // No data exists for the phone number
+            }
+        } catch (e: Exception) {
+            // Handle any exceptions that occur during the database operation
+            Log.e("Retrieve Temp User Data", "Error: ${e.message}")
+            null
+        }
+    }
+
+    fun findUserBy(){
 
     }
 
--- app/src/main/java/com/sroo/astroobus/view-model/RegisterViewModel.kt
+++ app/src/main/java/com/sroo/astroobus/view-model/RegisterViewModel.kt
@@ -8,13 +8,20 @@ import com.sroo.astroobus.repository.RegisterRepository
 
 class RegisterViewModel() {
     private val repository = RegisterRepository()
+    val emailRegex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$"
     val UIHelper = UIHelper()
 
-     fun registerUser(user: User, context: Context){
-        if(user.name == null|| user.password == null|| user.email == null|| user.phoneNum == null){
+     fun registerTempUser(name:String, email:String, password:String, confPass:String, phoneNum:String, context: Context){
+        if(name == null|| email == null || password == null|| confPass == null|| phoneNum == null){
             UIHelper.createToast(context, "All fields must be filled")
-        }else{
-
+        }else if(password.length < 8){
+            UIHelper.createToast(context, "Password must be atleast 8 characters")
+        }else if (!password.matches(Regex(".*[a-zA-Z].*")) || !password.matches(Regex(".*\\d.*"))){
+            UIHelper.createToast(context, "Password must be alphanumeric")
+        }else if (password != confPass){
+            UIHelper.createToast(context, "Password doesn't match")
+        }else if(email.matches(emailRegex.toRegex())){
+            UIHelper.createToast(context, "Invalid email")
         }
     }
 }
